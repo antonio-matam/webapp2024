ccc6185cdb08678e71ac0563e5fc02bc45ac3ef7
antonio-matam <antonio.matam@edu.uah.es>
2024-11-25 19:08:43 +0000

    webapp

---
+++ app/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+#  Programa: main.py
+# Propósito: Creación aplicación web Flask
+#     Autor: Óscar García
+#     Fecha: 09/12/2019 
+
+import flask
+import datetime
+import os
+
+# Crear el objeto que representa la aplicacion web
+APP = flask.Flask(__name__)
+nombre=os.environ['NAME']
+contador_visitas=0
+
+@APP.route('/hola')
+def index():
+    """ Muestra la página inicial asociada al recurso /
+        y que estará contenida en el archivo index.html
+    """
+    return "Hola Mundo"
+
+@APP.route('/adios')
+def index2():
+    """ Muestra la página inicial asociada al recurso /
+        y que estará contenida en el archivo index.html
+    """
+    return "Adios Mundo"
+
+@APP.route('/')
+def index3():
+    """ Muestra la página inicial asociada al recurso /
+        y que estará contenida en el archivo index.html
+    """
+    global contador_visitas
+    contador_visitas=contador_visitas+1
+    return flask.render_template('index.html',nombre=nombre, contador_visitas=contador_visitas,  timestamp=datetime.datetime.now())
+
+
+if __name__ == '__main__':
+    APP.debug = True
+    APP.run(host='0.0.0.0', port=os.environ['PORT'])
