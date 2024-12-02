364ff05c1bb853b1c04ce003f2e9332159c86ca2
antonio-matam <antonio.matam@edu.uah.es>
2024-12-02 18:39:52 +0000

    Añado redis

--- app/main.py
+++ app/main.py
@@ -8,11 +8,21 @@
 import flask
 import datetime
 import os
+import redis
 
 # Crear el objeto que representa la aplicacion web
 APP = flask.Flask(__name__)
 nombre=os.environ['NAME']
-contador_visitas=0
+
+
+#Crear el cliente para acceder a Redis
+redis_location=os.environ['REDIS_LOCATION']
+redis_port=os.environ['REDIS_PORT']
+redis_client = redis.Redis(host=redis_location, port=redis_port, db=0)
+
+PREFIX= "webapp2024"
+CONTADOR_BASE_KEY= "contador_visitas"
+CONTADOR_KEY="-".join([PREFIX,CONTADOR_BASE_KEY])
 
 @APP.route('/hola')
 def index():
@@ -33,8 +43,10 @@ def index3():
     """ Muestra la página inicial asociada al recurso /
         y que estará contenida en el archivo index.html
     """
-    global contador_visitas
-    contador_visitas=contador_visitas+1
+    #contador_visitas=contador_visitas+1
+
+    contador_visitas= redis_client.incr(CONTADOR_KEY);
+
     return flask.render_template('index.html',nombre=nombre, contador_visitas=contador_visitas,  timestamp=datetime.datetime.now())
 
 
